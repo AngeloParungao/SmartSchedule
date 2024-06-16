66a18ed25eb6610e664de382cad8c5930cb877d3
AngeloParungao <angeloparungao.ap@gmail.com>
2024-06-16 21:50:12 +0800

    scheduling

--- frontend/src/pages/scheduling.jsx
+++ frontend/src/pages/scheduling.jsx
@@ -1,30 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from '../assets/components/sidebar';
 import Navbar from '../assets/components/scheduling-navbar';
+import axios from 'axios';
+import { Toaster, toast } from 'react-hot-toast';
 import '../css/scheduling.css';
 
 function Scheduling() {
   const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const timesOfDay = ['7:00 - 8:00', '8:00 - 9:00', '9:00 - 10:00', '10:00 - 11:00', '11:00 - 12:00', '12:00 - 13:00', '13:00 - 14:00', '14:00 - 15:00', '15:00 - 16:00', '16:00 - 17:00', '17:00 - 18:00', '18:00 - 19:00', '19:00 - 20:00'];
+  const [sections, setSections] = useState([]);
+
+  useEffect(() => {
+    fetchSections();
+  }, []);
+
+  const fetchSections = async () => {
+    try {
+      const response = await axios.get('http://localhost:8082/api/sections/fetch');
+      setSections(response.data);
+    } catch (error) {
+      console.error('Error fetching sections:', error);
+      toast.error('Failed to fetch sections');
+    }
+  };
+
 
   return (
     <div>
+      <div>
+        <Toaster position="bottom-right" reverseOrder={false} />
+      </div>
       <Sidebar />
       <Navbar />
       <div className="scheduling-container">
         <div className="controls">
-            <div>
-                <label htmlFor="">Year & Section</label>
-                <select className="section-dropdown">
-                    <option>BSIT 3D - GROUP 1</option>
-                    {/* Add more options as needed */}
-                </select>
-            </div>
-            <div>
-                <button className="add-item-btn">Add Item</button>
-                <button className="edit-item-btn">Edit Item</button>
-                <button className="delete-item-btn">Delete Item</button>
-            </div>
+          <div>
+            <label htmlFor="">Year & Section</label>
+            <select className="section-dropdown">
+              {sections.map(section => (
+                <option key={section.section_id}>
+                  {section.section_name} - {section.section_group}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <button className="add-item-btn" onClick={openModal}>Add Item</button>
+            <button className="edit-item-btn">Edit Item</button>
+            <button className="delete-item-btn">Delete Item</button>
+          </div>
         </div>
         <div className="timetable">
           <table>
