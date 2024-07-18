555b67c0c2d0a75ee2760895db25e66402d1e64e
AngeloParungao <angeloparungao.ap@gmail.com>
2024-07-18 23:38:11 +0800

    database

--- backend/dbsmartschedule.sql
+++ backend/dbsmartschedule.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2024 at 05:13 PM
+-- Generation Time: Jul 18, 2024 at 05:37 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,45 @@ SET time_zone = "+00:00";
 --
 -- Database: `dbsmartschedule`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity`
+--
+
+CREATE TABLE `activity` (
+  `activity_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `action` varchar(10) NOT NULL,
+  `details` varchar(100) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `activity`
+--
+
+INSERT INTO `activity` (`activity_id`, `user_id`, `action`, `details`, `type`, `timestamp`) VALUES
+(37, 1, 'Add', 'test, test test', 'instructor', '2024-07-18 19:43:54'),
+(38, 1, 'Update', 'test, test test', 'instructor', '2024-07-18 19:44:07'),
+(39, 1, 'Delete', '1', 'instructor', '2024-07-18 19:44:19'),
+(40, 1, 'Add', 'test - Group 1', 'section', '2024-07-18 19:44:36'),
+(41, 1, 'Update', 'test - Group 1', 'section', '2024-07-18 19:44:44'),
+(42, 1, 'Delete', '1', 'section', '2024-07-18 19:44:50'),
+(43, 1, 'Add', 'test(test)', 'subject', '2024-07-18 19:45:09'),
+(44, 1, 'Update', 'sample(test)', 'subject', '2024-07-18 19:45:20'),
+(45, 1, 'Delete', '1', 'subject', '2024-07-18 19:45:24'),
+(46, 1, 'Add', 'test)', 'room', '2024-07-18 19:48:18'),
+(47, 1, 'Update', 'sample', 'room', '2024-07-18 19:49:04'),
+(48, 1, 'Delete', '1', 'room', '2024-07-18 19:49:16'),
+(49, 1, 'Add', 'BSIT 3D - Group 2', 'schedule', '2024-07-18 19:51:53'),
+(52, 1, 'Update', 'BSIT 3D - Group 2', 'schedule', '2024-07-18 20:02:07'),
+(53, 1, 'Update', 'BSIT 3D - Group 2', 'schedule', '2024-07-18 20:02:22'),
+(54, 1, 'Delete', '1', 'schedule', '2024-07-18 20:06:30'),
+(55, 1, 'Add', 'BSIT 3D - Group 2', 'schedule', '2024-07-18 20:06:57'),
+(56, 1, 'Update', 'BSIT 3D - Group 2', 'schedule', '2024-07-18 20:07:14');
 
 -- --------------------------------------------------------
 
@@ -47,11 +86,7 @@ INSERT INTO `instructors` (`instructor_id`, `email`, `firstname`, `middlename`, 
 (23, 'clyde@gmail.com', 'Clyde', '', 'Mondero', 'Part-timer', 'Web and Mobile Development', 1),
 (25, 'danrick@gmail.com', 'Danrick Salamat', 'Conception', 'Macatanong', 'Part-timer', 'Web and Mobile Development', 1),
 (26, 'albert@gmail.com', 'Albert', '', 'Junio', 'Part-timer', 'Business Analytics', 1),
-(27, 'erick@gmail.com', 'Erick', '', 'Caleja', 'part-timer', 'Business Analytics', 1),
-(28, 'andrei@gmail.com', 'Andrei John', '', 'Poma', 'Part-timer', 'Web and Mobile Development', 1),
-(29, 'maika@gmail.com', 'Maika', '', 'Ybiza', 'regular', 'Business Analytics', 1),
-(30, 'maybilene@gmail.com', 'Maybilene', '', 'Bonifacio', 'Part-timer', 'Business Analytics', 1),
-(34, 'test@gmail.com', 'test', 'test', 'test', 'Regular', 'sample', 1);
+(27, 'erick@gmail.com', 'Erick', '', 'Caleja', 'part-timer', 'Business Analytics', 1);
 
 -- --------------------------------------------------------
 
@@ -100,14 +135,14 @@ CREATE TABLE `schedules` (
 --
 
 INSERT INTO `schedules` (`schedule_id`, `instructor`, `subject`, `section_name`, `section_group`, `class_type`, `room`, `background_color`, `day`, `start_time`, `end_time`, `creator_id`) VALUES
-(52, 'Albert  Junio', 'Web Development', 'BSIT 3D', 'Group 2', 'Lecture', 'RM201', '#abeeaa', 'Monday', '07:00:00', '09:00:00', 1),
 (53, 'Danrick Salamat Conception Macatanong', 'Quality Assurance', 'BSIT 3D', 'Group 2', 'Lecture', 'RM201', '#f5a3a3', 'Monday', '12:00:00', '14:00:00', 1),
 (54, 'Albert  Junio', 'Web Development', 'BSIT 3D', 'Group 2', 'Laboratory', 'MML', '#aff8a5', 'Tuesday', '13:00:00', '16:00:00', 1),
 (55, 'Clyde  Mondero', 'System Analysis and Design', 'BSIT 3D', 'Group 2', 'Lecture', 'RM201', '#fdbffb', 'Wednesday', '16:00:00', '18:00:00', 1),
 (56, 'Danrick Salamat Conception Macatanong', 'Quality Assurance', 'BSIT 3D', 'Group 2', 'Laboratory', 'MML', '#f3afaf', 'Thursday', '17:00:00', '20:00:00', 1),
 (57, 'Clyde  Mondero', 'System Analysis and Design', 'BSIT 3D', 'Group 2', 'Laboratory', 'RM201', '#f0b7e5', 'Friday', '07:00:00', '10:00:00', 1),
 (58, 'Erick  Caleja', 'Object Oriented Programming', 'BSIT 3D', 'Group 2', 'Lecture', 'RM201', '#ffffff', 'Thursday', '07:00:00', '09:00:00', 1),
-(60, 'Erick  Caleja', 'Object Oriented Programming', 'BSIT 3D', 'Group 2', 'Laboratory', 'MML', '#ffffff', 'Tuesday', '08:00:00', '11:00:00', 1);
+(61, 'Erick  Caleja', 'Object Oriented Programming', 'BSIT 3D', 'Group 2', 'Laboratory', 'MML', '#ffffff', 'Tuesday', '08:00:00', '11:00:00', 1),
+(62, 'Albert  Junio', 'Web Development', 'BSIT 3D', 'Group 2', 'Lecture', 'RM201', '#b1fbb3', 'Monday', '07:00:00', '09:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -133,8 +168,7 @@ INSERT INTO `sections` (`section_id`, `section_name`, `section_group`, `year_lvl
 (4, 'BSIT 3D', 'Group 2', '3rd Year', 23, '', 1),
 (11, 'BSIT 3D', 'Group 1', '3rd Year', 17, '', 1),
 (12, 'BSIT 1A', 'Group 2', '1st Year', 20, '', 1),
-(13, 'BSIT 1A', 'Group 1', '1st Year', 25, '', 1),
-(14, 'BSIT 1B', 'Group 1', '1st Year', 20, '', 1);
+(13, 'BSIT 1A', 'Group 1', '1st Year', 25, '', 1);
 
 -- --------------------------------------------------------
 
@@ -189,6 +223,12 @@ INSERT INTO `users` (`user_id`, `email`, `password`) VALUES
 --
 
 --
+-- Indexes for table `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`activity_id`);
+
+--
 -- Indexes for table `instructors`
 --
 ALTER TABLE `instructors`
@@ -229,34 +269,40 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `activity`
+--
+ALTER TABLE `activity`
+  MODIFY `activity_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
 -- AUTO_INCREMENT for table `instructors`
 --
 ALTER TABLE `instructors`
-  MODIFY `instructor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `instructor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `schedules`
 --
 ALTER TABLE `schedules`
-  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT for table `sections`
 --
 ALTER TABLE `sections`
-  MODIFY `section_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `section_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `users`
